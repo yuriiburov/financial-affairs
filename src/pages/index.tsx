7d9c35c4c92d442626edff8ff233c6f2bffda1aa
yuriiburov <burovyuriireact@gmail.com>
2023-01-18 02:19:18 +0200

    add click handler on main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,28 @@
 import FinanceInShort from '@/components/financeInShort/FinanceInShort'
 import Ripples from '@/components/ripples/Ripples'
+import useAppTheme from '@/hooks/useAppTheme'
+import { Typography } from '@mui/material'
 
-export default function Index() {
+const Index = () => {
+  const { color } = useAppTheme()
+
   return (
     <>
+      <div
+        className='main__click-area'
+        onClick={() => {
+          console.log('click')
+        }}
+      />
       <FinanceInShort />
       <Ripples />
+      <div className='main-hint'>
+        <Typography color={color} fontSize={14}>
+          Натисніть будь-де
+        </Typography>
+      </div>
     </>
   )
 }
+
+export default Index
